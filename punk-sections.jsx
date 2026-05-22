d1c624c7dc61471e2ddecb54c57505a158f76894
DiegoAndresVega <diegoa.vegsil@gmail.com>
2026-05-22 21:21:31 +0200

    video

--- punk-sections.jsx
+++ punk-sections.jsx
@@ -167,7 +167,7 @@ function Hero() {
 }
 
 /* ───────── CONQUIS ───────── */
-const CONQUIS_VIDEO = ""; // ← pega aquí el embed URL de YouTube cuando lo tengas
+const CONQUIS_VIDEO = "https://www.youtube.com/embed/AVsR5lB--sw?si=sd5M-ZC0hCxgvyCg";
 
 function Conquis() {
   return (
